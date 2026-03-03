11ea7239061671a301ed9ee9d5c6e05194d0af19
Zackcodder <zachtech56@gmail.com>
2026-03-03 15:52:45 +0100

    modified:   lib/src/features/profile/widgets/faq_tile.dart

--- lib/src/features/profile/widgets/faq_tile.dart
+++ lib/src/features/profile/widgets/faq_tile.dart
@@ -7,7 +7,8 @@ class FAQTile extends StatelessWidget {
   final int index;
   final HelpAndSupportController helpAndSupportController;
 
-  const FAQTile({super.key, required this.index, required this.helpAndSupportController});
+  const FAQTile(
+      {super.key, required this.index, required this.helpAndSupportController});
 
   @override
   Widget build(BuildContext context) {
@@ -31,16 +32,23 @@ class FAQTile extends StatelessWidget {
                     ),
                   ),
                   Transform.rotate(
-                    angle: helpAndSupportController.isExpanded(index) ? -pi / 2.0 : pi / 2.0,
-                    child: SvgPicture.asset(Assets.assetsArrowRightCurved, colorFilter: ColorFilter.mode(
-                      Theme.of(context).brightness == Brightness.dark
-                          ? AppColor.grey200 : AppColor.grey400,
-                      BlendMode.srcIn, )),
+                    angle: helpAndSupportController.isExpanded(index)
+                        ? -pi / 2.0
+                        : pi / 2.0,
+                    child: SvgPicture.asset(Assets.assetsArrowRightCurved,
+                        colorFilter: ColorFilter.mode(
+                          Theme.of(context).brightness == Brightness.dark
+                              ? AppColor.grey200
+                              : AppColor.grey400,
+                          BlendMode.srcIn,
+                        )),
                   ),
                 ],
               ),
             ),
-            MediaQuery.of(context).size.height < 670 ? const SizedBox.shrink() : AppSizing.h24,
+            MediaQuery.of(context).size.height < 670
+                ? const SizedBox.shrink()
+                : AppSizing.h24,
             if (helpAndSupportController.isExpanded(index))
               buildAnswerText(context, helpAndSupportController.answers[index]),
           ],
@@ -48,41 +56,102 @@ class FAQTile extends StatelessWidget {
       },
     );
   }
+
   Widget buildAnswerText(BuildContext context, String answer) {
-    final List<String> parts = answer.split('www.intellyjent.com');
+    // Regex pattern to detect URLs in text
+    final urlPattern = RegExp(
+      r'(https?://[^\s]+)|(www\.[^\s]+)',
+      caseSensitive: false,
+    );
+
+    final matches = urlPattern.allMatches(answer);
+
+    if (matches.isEmpty) {
+      // No URLs found, return simple text
+      return Align(
+        alignment: Alignment.centerLeft,
+        child: Column(
+          children: [
+            Text(
+              answer,
+              style: TextStyle(
+                fontSize: MediaQuery.of(context).size.width < 800 ? null : 20,
+                color: Get.theme.brightness == Brightness.dark
+                    ? Colors.white
+                    : AppColor.appBlack,
+                fontWeight: FontWeight.w400,
+              ),
+            ),
+            AppSizing.h28,
+          ],
+        ),
+      );
+    }
+
+    // Build rich text with clickable URLs
+    final List<TextSpan> textSpans = [];
+    int lastEnd = 0;
+
+    for (final match in matches) {
+      // Add text before the URL
+      if (match.start > lastEnd) {
+        textSpans.add(TextSpan(
+          text: answer.substring(lastEnd, match.start),
+          style: TextStyle(
+            fontSize: MediaQuery.of(context).size.width < 800 ? null : 20,
+            color: Get.theme.brightness == Brightness.dark
+                ? Colors.white
+                : AppColor.appBlack,
+            fontWeight: FontWeight.w400,
+          ),
+        ));
+      }
+
+      final String url = match.group(0)!;
+
+      // Determine which launcher to use based on URL
+      final bool isLaunchpad = url.toLowerCase().contains('launchpad');
+
+      textSpans.add(TextSpan(
+        text: url,
+        style: TextStyle(
+          fontSize: MediaQuery.of(context).size.width < 800 ? null : 20,
+          color: Colors.blue,
+          decoration: TextDecoration.underline,
+        ),
+        recognizer: TapGestureRecognizer()
+          ..onTap = () {
+            if (isLaunchpad) {
+              helpAndSupportController.openLaunchpad();
+            } else {
+              helpAndSupportController.launchWeb();
+            }
+          },
+      ));
+
+      lastEnd = match.end;
+    }
+
+    // Add remaining text after last URL
+    if (lastEnd < answer.length) {
+      textSpans.add(TextSpan(
+        text: answer.substring(lastEnd),
+        style: TextStyle(
+          fontSize: MediaQuery.of(context).size.width < 800 ? null : 20,
+          color: Get.theme.brightness == Brightness.dark
+              ? Colors.white
+              : AppColor.appBlack,
+          fontWeight: FontWeight.w400,
+        ),
+      ));
+    }
+
     return Align(
       alignment: Alignment.centerLeft,
       child: Column(
         children: [
           RichText(
-            text: TextSpan(
-              children: [
-                TextSpan(
-                  text: parts[0],
-                  style: TextStyle(
-                      fontSize: MediaQuery.of(context).size.width < 800 ? null : 20,
-                      color: Get.theme.brightness == Brightness.dark
-                          ? Colors.white
-                          : AppColor.appBlack,
-                      fontWeight: FontWeight.w400,
-                  ),
-                ),
-                if (parts.length > 1)
-                  TextSpan(
-                    text: 'www.intellyjent.com',
-                    style: TextStyle(
-                      fontSize: MediaQuery.of(context).size.width < 800 ? null : 20,
-                      color: Colors.blue,
-                      decoration: TextDecoration.underline,
-                    ),
-                    recognizer: TapGestureRecognizer()
-                      ..onTap = () {
-                        helpAndSupportController.launchWeb();
-                      },
-                  ),
-                if (parts.length > 1) TextSpan(text: parts[1]),
-              ],
-            ),
+            text: TextSpan(children: textSpans),
           ),
           AppSizing.h28,
         ],
